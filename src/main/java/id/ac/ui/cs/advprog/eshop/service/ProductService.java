5482f674193490348df35db49d0792df6699baff
Reyhan Ardian <reyhanardian16@gmail.com>
2026-02-23 21:35:07 +0700

    refactor: some refactoring on templates and repository attr

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -5,9 +5,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public Product edit(Product product);
-    public void delete(String productId);
-    public List<Product> findAll();
-    public Product findById(String productId);
+    Product create(Product product);
+    Product edit(Product product);
+    void delete(String productId);
+    List<Product> findAll();
+    Product findById(String productId);
 }
